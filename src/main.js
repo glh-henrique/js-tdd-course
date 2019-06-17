5ba6de2170226ee824f34b9243116aa8b88dfd06
Guilherme Henrique Ricardo <guilherme.ricardo@hbsis.com.br>
2019-06-17 14:53:30 -0300

    teste apra calculadora

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log("asdasd");
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => ((num2 === 0) ? 'Não é possivel divisão por zero!' : num1 / num2);
+
+export {
+  sum, sub, mult, div,
+};
